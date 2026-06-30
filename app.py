caf2d761829e9d7a243ced65748f32770e9e50d5
Pranathi Kurapati <pranathikurapati282@gmail.com>
2026-06-30 19:23:50 +0530

    Fix Render deployment

--- app.py
+++ app.py
@@ -25,8 +25,9 @@ demo = gr.Interface(
     The portal will activate in real time.
     """
 )
+import os
 
 demo.launch(
     server_name="0.0.0.0",
-    server_port=7860
+    server_port=int(os.environ.get("PORT", 7860))
 )
