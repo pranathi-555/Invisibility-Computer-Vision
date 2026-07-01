94a582c01bda35bfaf59a4fae67e6f8af84be8a2
Pranathi Kurapati <pranathikurapati282@gmail.com>
2026-07-01 14:58:01 +0530

    Debug mediapipe import

--- engine.py
+++ engine.py
@@ -38,7 +38,13 @@ class HandTracker:
     def __init__(self):
         import mediapipe as mp
 
-        
+        print("=" * 50)
+        print("MediaPipe file:", mp.__file__)
+        print("MediaPipe version:", getattr(mp, "__version__", "unknown"))
+        print("Has solutions:", hasattr(mp, "solutions"))
+        print("Members:", dir(mp))
+        print("=" * 50)
+
         self.mp_hands = mp.solutions.hands
 
         self.hands = self.mp_hands.Hands(
